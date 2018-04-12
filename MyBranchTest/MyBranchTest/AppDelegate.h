4fa282f08bea1d0e3fa50bf44acfe34315ddbecf
shmysteel <shmysteel@shmysteeldeMacBook-Pro.local>
2018-04-12 15:19:48 +0800

    创建工程

---
+++ MyBranchTest/MyBranchTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyBranchTest
+//
+//  Created by shmysteel on 2018/4/12.
+//  Copyright © 2018年 lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyBranchTest/MyBranchTest/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  MyBranchTest
+//
+//  Created by shmysteel on 2018/4/12.
+//  Copyright © 2018年 lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
